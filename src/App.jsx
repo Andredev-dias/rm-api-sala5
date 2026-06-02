3f93b54965fbf166b8b95fa0fc32c9ed8e25b1d6
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-06-02 08:44:37 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import './App.module.css'
+import s from './App.module.css'
+import { useEffect, useState } from 'react'
+import { api } from './constants/api'
+
+import logo from '/logo.png'
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    api.get(`/character`).then((response) => {
+        setData(response.data.results)
+    }).catch((error) => {
+      console.error("Deu ruim!!!", error)
+    })
+  }, [])
+  
 
   return (
     <>
-      
+      <img className={s.logo} src={logo} alt="Logo" />
+      <main>
+        {data.map((item, index) => {
+          return(
+            <div key={index}>
+                <img src={item.image} alt={item.name} />
+                <h4>Name: {item.name}</h4>
+                <p>Species: {item.species}</p>
+                {item.status === "Dead" ? "Status:🧟" : item.status === "Alive" ? "Status: 😊" : <p>Status: {item.status}</p>}
+                <p>Origin: {item.origin.name}</p>
+            </div>
+          )
+        })}
+      </main>
     </>
   )
 }
